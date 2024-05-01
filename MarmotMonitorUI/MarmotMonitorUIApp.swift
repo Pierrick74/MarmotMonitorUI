e4435a5be2f58e1e80460d1bb88fdd9b0c3bb99d
pierrick <pierrickviret@gmail.com>
2024-05-01 21:21:57 +0200

    Initial Commit

---
+++ MarmotMonitorUI/MarmotMonitorUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarmotMonitorUIApp.swift
+//  MarmotMonitorUI
+//
+//  Created by pierrick viret on 01/05/2024.
+//
+
+import SwiftUI
+
+@main
+struct MarmotMonitorUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
